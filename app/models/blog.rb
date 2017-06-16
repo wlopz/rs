af0d56d7d1f7b045cfd6b112951ac754f6bb345a
Wilfredo Lopez <jrlopez85@gmail.com>
2017-06-16 14:28:34 -0700

    Work on blog content. Posts are showing.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,8 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  scope :most_recent, -> {order(id: :desc)}
+
   def should_generate_new_friendly_id?
     title_changed?
   end
